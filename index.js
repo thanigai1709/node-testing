edd14529c457a55b3b4f79d17ab2b2926b64c7ff
Thanigai <thanigai@excrin.com>
2021-09-10 17:12:43 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.port || 3000;
+
+app.get("/", (req, res) => {
+	res.send("Welcome to home page");
+});
+
+app.listen(port, () => {
+	console.log(`server running at port ${port}`);
+});
