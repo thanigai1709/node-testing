54b93da34926cde5c516a523b964d384c5ebfe64
Thanigai <thanigai@excrin.com>
2021-09-10 17:46:49 +0530

    new route added

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get("/", (req, res) => {
 	res.send("Welcome to home");
 });
 
+app.get("/users", (req, res) => {
+	fetch("https://jsonplaceholder.typicode.com/users")
+		.then((response) => response.json())
+		.then((json) => res.send(json));
+});
+
 app.listen(port, () => {
 	console.log(`server running at port ${port}`);
 });
