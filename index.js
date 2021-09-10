e12f9cbf77ff08b73fc11f56f862f097d5517e0f
Thanigai <thanigai@excrin.com>
2021-09-10 17:44:04 +0530

    edited

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.port || 3000;
 
 app.get("/", (req, res) => {
-	res.send("Welcome to home page");
+	res.send("Welcome to home");
 });
 
 app.listen(port, () => {
